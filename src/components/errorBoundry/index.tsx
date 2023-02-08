4cece421df3bd6912251f831aedf39df001a83a6
arman-ag <alighanbari.1994@gmail.com>
2023-02-08 09:44:15 +0330

    docs: Add header to single page & add google tag, meta tags

--- src/components/errorBoundry/index.tsx
+++ src/components/errorBoundry/index.tsx
@@ -7,22 +7,16 @@ class ErrorBoundary extends React.Component {
   constructor(props: React.ReactNode) {
     super(props);
 
-    // Define a state variable to track whether is an error or not
     this.state = { hasError: false };
   }
   static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI
-
     return { hasError: true };
   }
   componentDidCatch(error, errorInfo) {
-    // You can use your own error logging service here
     console.log({ error, errorInfo });
   }
   render() {
-    // Check if the error is thrown
     if (this.state.hasError) {
-      // You can render any custom fallback UI
       return (
         <div className=' flex justify-center items-center flex-col grow 	h-screen'>
           <Image alt='404 image' src={error} className='w-72 ' />
@@ -30,8 +24,6 @@ class ErrorBoundary extends React.Component {
         </div>
       );
     }
-
-    // Return children components in case of no error
 
     return this.props.children;
   }
